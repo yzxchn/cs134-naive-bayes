884011c2d4b3a8ae941684cae5644b69c050dca6
Yuzhe <yzchen@brandeis.edu>
2016-09-19 20:37:39 -0400

    can pass baseline tests but slow

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,7 @@
 # -*- mode: Python; coding: utf-8 -*-
 from collections import Counter
 from classifier import Classifier
+import numpy as np
 import math
 
 class NaiveBayes(Classifier):
@@ -12,58 +13,52 @@ class NaiveBayes(Classifier):
     def set_model(self, model): self.seen = model
     model = property(get_model, set_model)
 
-    def train(self, training_set):
-        self.model = {}
-        class_count = Counter()
-        featureset = set()
-        for d in training_set:
-            if d.label not in self.model:
-                self.model[d.label] = Counter()
-            class_count[d.label] += 1
-            for f in set(d.features()):
-                featureset.add(f)
-                self.model[d.label][f] += 1
+    def train(self, train_set):
+        count_table = {}
+        label_to_index = self.get_label_index(train_set)
+        label_count = np.zeros(len(label_to_index))
+        for doc in train_set:
+            l = doc.label
+            if len(l) > 0:
+                label_count[label_to_index[l]] += 1
+                for feature in set(doc.features()):
+                    if feature not in count_table:
+                        count_table[feature] = np.ones(len(label_to_index))
+                    count_table[feature][label_to_index[l]] += 1
 
-        labels = self.model.keys()
+        for feature in count_table.keys():
+            for label in label_to_index.keys():
+                count_table[feature][label_to_index[label]] =  math.log(count_table[feature][label_to_index[label]] / \
+                                                                        (label_count[label_to_index[label]] + 2.0))
+        count_table["*-UNK-*"] = np.zeros(len(label_to_index))
+        for label in label_to_index.keys():
+            count_table["*-UNK-*"][label_to_index[label]] = math.log(1.0 / (label_count[label_to_index[label]] + 2.0))
 
-        self.model["*-PRIORS-*"] = {}
-        self.model["*-VOCAB-*"] = featureset
-        self.model["*-CLSCOUNT-*"] = class_count
+        priors = np.log(label_count / (1.0*len(train_set)))
+        cond_prob_table = count_table
+        feature_set = count_table.keys()
 
-        for l in labels:
-            print("{0} {1}".format(l, class_count[l]))
-            l_prior = math.log(1.0*class_count[l] / len(training_set))
-            self.model["*-PRIORS-*"][l] = l_prior
-            for f in self.model[l].keys():
-                self.model[l][f] = math.log(1.0*(self.model[l][f] + 1) / (class_count[l] + len(featureset)))
+        self.model = (cond_prob_table, priors, label_to_index, feature_set, label_count)
 
-
-    def classify(self, doc):
-        print("testing")
-        max_prob = 0.0
-        max_label = None
-        featureset = self.model["*-VOCAB-*"]
-        class_count = self.model["*-CLSCOUNT-*"]
-        for l in self.model["*-PRIORS-*"].keys():
-            prob_sum = self.model["*-PRIORS-*"][l]
-            for f in doc.features():
-                if f not in self.model[l].keys():
-                    prob_sum += math.log(1.0 / (class_count[l] + len(featureset)))
+    def classify(self, instance):
+        cond_prob_table, priors, label_to_index, feature_set, label_count = self.model
+        results = []
+        for label in label_to_index.keys():
+            prob_sum = priors[label_to_index[label]]
+            for feature in set(instance.features()):
+                if feature not in feature_set:
+                    prob_sum += cond_prob_table["*-UNK-*"][label_to_index[label]]
                 else:
-                    prob_sum += self.model[l][f]
-            if prob_sum >= max_prob:
-                max_prob = prob_sum
-                max_label = l
-        return max_label
-
+                    prob_sum += cond_prob_table[feature][label_to_index[label]]
+            results.append((label, prob_sum))
+        return max(results, key=lambda (x, y): y)[0]
 
     def get_label_index(self, dataset):
         labels = {}
         i = 0
         for d in dataset:
-            if d.label not in labels:
+            if d.label not in labels and len(d.label) > 0:
                 labels[d.label] = i
                 i += 1
         return labels
 
-c = NaiveBayes()
